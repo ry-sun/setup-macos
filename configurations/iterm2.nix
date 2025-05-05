0564d91a6b6a814ebe252b891fc336e4d6d27ac1
Ruiyang Sun <ruiyangsun02@gmail.com>
2025-05-05 21:47:37 +0800

    fix: iterm2 do not copy `.iterm2_shell_ingegration.zsh`

--- configurations/iterm2.nix
+++ configurations/iterm2.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 {
   home.file.".config/iterm2/com.googlecode.iterm2.plist".source =
-    ../dotfiles/com.googlecode.iterm2.plist;
+    ../dotfiles/iterm2/com.googlecode.iterm2.plist;
+
+  home.file.".iterm2_shell_integration.zsh" = {
+    source = ../dotfiles/iterm2/.iterm2_shell_integration.zsh;
+    executable = true;
+  };
 }
