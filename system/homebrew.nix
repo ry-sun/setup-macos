fd8732c817900dae6bbabe72cc49af98d701adde
Ruiyang Sun <ruiyangsun02@gmail.com>
2025-05-05 22:06:21 +0800

    fix: uptap brew core and cask, cause user should use API directly to tap them manually

--- system/homebrew.nix
+++ system/homebrew.nix
@@ -10,16 +10,19 @@
 
   ## homebrew taps
   homebrew.taps = [
-    {
-      name = "homebrew/core";
-      clone_target = "https://github.com/Homebrew/homebrew-core";
-      force_auto_update = true;
-    }
-    {
-      name = "homebrew/cask";
-      clone_target = "https://github.com/Homebrew/homebrew-cask";
-      force_auto_update = true;
-    }
+    # Cueeently, brew officially recommends to use API instead of tap core and cask,
+    # if you want to contribute to brew, you can tap core or cask manually and than
+    # uncomment the following lines.
+    # {
+    #   name = "homebrew/core";
+    #   clone_target = "https://github.com/Homebrew/homebrew-core";
+    #   force_auto_update = true;
+    # }
+    # {
+    #   name = "homebrew/cask";
+    #   clone_target = "https://github.com/Homebrew/homebrew-cask";
+    #   force_auto_update = true;
+    # }
     {
       name = "homebrew/command-not-found";
       clone_target = "https://github.com/Homebrew/homebrew-command-not-found";
