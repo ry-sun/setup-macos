13ef5ad6dc4373606b786cc7fcdd556986f166b2
Ruiyang Sun <ruiyangsun02@gmail.com>
2025-11-30 21:55:33 +0800

    feat: update ssh config to match lastest version of homemanager

--- configurations/ssh.nix
+++ configurations/ssh.nix
@@ -1,13 +1,26 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
       "github.com" = {
         hostname = "ssh.github.com";
         port = 443;
         user = "git";
         identityFile = "~/.ssh/github-key";
+      };
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
       };
     };
   };
